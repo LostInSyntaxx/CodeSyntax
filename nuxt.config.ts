a99be3e3e8096f45c6a5a4cd21fb626924dbfe11
LostInSyntaxx <poomabcde11@gmail.com>
2026-02-14 17:08:07 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
-  
-  sourcemap: {
-    server: false,
-    client: false,
-  },
-  
   runtimeConfig: {
     spotify: {
       clientId: process.env.SPOTIFY_CLIENT_ID || '',
